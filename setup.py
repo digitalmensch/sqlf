44d2aea7bb9225d4ea8b2ec8e6caf3692d2f1c28
Tobias Ammann <tobias.ammann@adnm.ch>
2018-03-31 01:22:26 +0200

    Fix trove classifier

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Only',
+        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Database :: Front-Ends',
         'Operating System :: OS Independent',
     ],
