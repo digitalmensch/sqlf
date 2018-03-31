2977ddeb872fad8a6ebdf4e5e78f6397eb9287e0
Tobias Ammann <tobias.ammann@adnm.ch>
2018-03-31 11:10:42 +0200

    Ported code and tests from sqlfunc

--- sqlf/__init__.py
+++ sqlf/__init__.py
@@ -5,3 +5,5 @@
 __author__ = """Digitalmensch"""
 __email__ = 'contact@digitalmensch.ch'
 __version__ = '0.2.4'
+
+from .sqlfunc import sqldb
--- tests/test_sqlf.py
+++ tests/test_sqlf.py
@@ -4,22 +4,58 @@
 """Tests for `sqlf` package."""
 
 import pytest
+import sqlf
 
 
-from sqlf import sqlf
+db = sqlf.sqldb('''
+    CREATE TABLE IF NOT EXISTS users (
+        userid   INTEGER PRIMARY KEY,
+        username TEXT    UNIQUE NOT NULL,
+        bcrypt   BLOB    NOT NULL
+    );
+''', database=':memory:') # this is the default
 
+@db.sqludf
+def bcrypt_hash(password):
+    # call to library here
+    return b'$2b$12$.OjbRwRejxw92C89sA6JkOVrhmQzGsjoyCf1ofIN9hUNdHFufb3ty'
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+@db.sqludf
+def bcrypt_verify(password, bcrypthash):
+    # call to library here
+    return 'password123' == password
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+@db.sqlfunc
+def add_user(username, password):
+    ''' INSERT OR IGNORE INTO users (username, bcrypt)
+        VALUES (:username, bcrypt_hash(:password));
+    '''
 
+@db.sqlfunc(post=bool, single=True, default=False)
+def login(username, password):
+    ''' SELECT 1 FROM users
+        WHERE username=:username
+          AND bcrypt_verify(:password, bcrypt);
+    '''
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+@db.sqlfunc
+def list_users():
+    ''' SELECT userid, username FROM users;
+    '''
+
+def test_scenario():
+    '''Test script ported from sqlfunc'''
+
+    add_user('root', 'password123')
+
+    x1 = login('root', 'secret')
+    print('x1', x1)
+    assert False == x1
+
+    x2 = login('root', 'password123')
+    print('x2', x2)
+    assert True == x2
+
+    x3 = list_users()
+    print('x3', x3)
+    assert len(x3) == 1
