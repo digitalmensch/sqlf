451167095fb053108f45d9a74fd15cb57f1a0e8e
Tobias Ammann <tobias.ammann@adnm.ch>
2018-04-03 23:44:47 +0200

    Remove old sqlfunc code

--- sqlf/__init__.py
+++ sqlf/__init__.py
@@ -6,6 +6,5 @@ __author__ = """Digitalmensch"""
 __email__ = 'contact@digitalmensch.ch'
 __version__ = '0.2.6'
 
-from .sqlfunc import sqldb
 from .sqlf import sql
 from .sqlf import scalar_udf
--- tests/test_sqlf.py
+++ tests/test_sqlf.py
@@ -6,68 +6,67 @@
 import pytest
 import sqlf
 
-################################################################################
-# Old API (sqlfunc)
-################################################################################
-
-db = sqlf.sqldb('''
-    CREATE TABLE IF NOT EXISTS users (
-        userid   INTEGER PRIMARY KEY,
-        username TEXT    UNIQUE NOT NULL,
-        bcrypt   BLOB    NOT NULL
-    );
-''', database=':memory:') # this is the default
-
-@db.sqludf
-def bcrypt_hash(password):
-    # call to library here
-    return b'$2b$12$.OjbRwRejxw92C89sA6JkOVrhmQzGsjoyCf1ofIN9hUNdHFufb3ty'
-
-@db.sqludf
-def bcrypt_verify(password, bcrypthash):
-    # call to library here
-    return 'password123' == password
-
-@db.sqlfunc
-def add_user(username, password):
-    ''' INSERT OR IGNORE INTO users (username, bcrypt)
-        VALUES (:username, bcrypt_hash(:password));
-    '''
-
-@db.sqlfunc(post=bool, single=True, default=False)
-def login(username, password):
-    ''' SELECT 1 FROM users
-        WHERE username=:username
-          AND bcrypt_verify(:password, bcrypt);
-    '''
-
-@db.sqlfunc
-def list_users():
-    ''' SELECT userid, username FROM users;
-    '''
-
-def test_scenario():
-    '''Test script ported from sqlfunc'''
-
-    add_user('root', 'password123')
-
-    x1 = login('root', 'secret')
-    print('x1', x1)
-    assert False == x1
-
-    x2 = login('root', 'password123')
-    print('x2', x2)
-    assert True == x2
-
-    x3 = list_users()
-    print('x3', x3)
-    assert len(x3) == 1
 
 ################################################################################
 # New API (sqlf)
 ################################################################################
 
-def test_new():
+def test_old_scenario():
+    '''Test script ported from sqlfunc'''
+
+    @sqlf.sql()
+    def setup():
+        '''
+            CREATE TABLE IF NOT EXISTS users (
+                userid   INTEGER PRIMARY KEY,
+                username TEXT    UNIQUE NOT NULL,
+                bcrypt   BLOB    NOT NULL
+            );
+        '''
+
+    @sqlf.scalar_udf
+    def bcrypt_hash(password):
+        return b'$2b$12$.OjbRwRejxw92C89sA6JkOVrhmQzGsjoyCf1ofIN9hUNdHFufb3ty'
+
+    @sqlf.scalar_udf
+    def bcrypt_verify(password, bcrypthash):
+        return 'password123' == password
+
+    @sqlf.sql()
+    def add_user(username, password):
+        ''' INSERT OR IGNORE INTO users (username, bcrypt)
+            VALUES (:username, bcrypt_hash(:password));
+        '''
+
+    @sqlf.sql()
+    def login(username, password):
+        ''' SELECT 1 FROM users
+            WHERE username=:username
+              AND bcrypt_verify(:password, bcrypt);
+        '''
+
+    @sqlf.sql()
+    def list_users():
+        ''' SELECT userid, username FROM users;
+        '''
+
+    x0 = list(setup())
+    assert len(x0) == 0
+
+    x1 = list(add_user('root', 'password123'))
+    assert len(x1) == 0
+
+    x2 = list(login('root', 'secret'))
+    assert len(x2) == 0
+
+    x3 = list(login('root', 'password123'))
+    assert len(x3) == 1
+
+    x4 = list(list_users())
+    assert len(x4) == 1
+
+
+def test_new_scenario():
     @sqlf.sql()
     def _setup():
         ''' create table tests (a, b, c, d); '''
@@ -84,6 +83,7 @@ def test_new():
     assert 0 == len(list(_query()))
     assert 0 == len(list(_insert(1, b=3)))
     assert 1 == len(list(_query()))
+
 
 def test_scalar_udf():
     @sqlf.scalar_udf
