f4258268f02583217a1f83dda870a70e94ee8319
Tobias Ammann <tobias.ammann@adnm.ch>
2018-04-03 12:39:30 +0200

    Add UDF number

--- sqlf/udf_lib.py
+++ sqlf/udf_lib.py
@@ -1,5 +1,12 @@
-import typeguard
+# -*- coding: utf-8 -*-
 
+''' SQL in F(unctions)
+
+    udf_lib contains useful user-defined functions
+'''
+
+import typeguard
+import typing
 
 ################################################################################
 # Text Processing
@@ -10,3 +17,14 @@ def match(string1 : str, string2 : str) -> bool:
     import re
     return list(re.findall('[a-z0-9]+', string1.lower())) == \
            list(re.findall('[a-z0-9]+', string2.lower()))
+
+@typeguard.typechecked
+def number(string : str) -> typing.Union[int, float, None]:
+    import re
+    for tmp in re.finditer('\d+(\.\d+)?', string):
+        tmp = tmp.group(0)
+        if '.' in tmp:
+            return float(tmp)
+        else:
+            return int(tmp)
+        break
--- tests/test_udf_lib.py
+++ tests/test_udf_lib.py
@@ -1,4 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `sqlf` package."""
+
 import sqlf.udf_lib
+
 
 ################################################################################
 # Text Processing
@@ -8,3 +14,8 @@ def test_match():
     assert sqlf.udf_lib.match('this is a test', ' this is a test')
     assert sqlf.udf_lib.match('This is a Test', 'this IS  a test')
     assert sqlf.udf_lib.match('This-is-a-Test', 'this_is.a.test')
+
+def test_number():
+    assert 8 == sqlf.udf_lib.number('8 deg')
+    assert 8.5 == sqlf.udf_lib.number('@ 8.5 eur')
+    assert None == sqlf.udf_lib.number('@ _ eur')
